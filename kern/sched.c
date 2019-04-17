2dce3a0bcc5692a88d0a9bd149868627354c364b
YuanweiZHANG <veronica_zhang@pku.edu.cn>
2019-04-17 09:16:49 -0700

    Lab 4 Exercise 6

--- kern/sched.c
+++ kern/sched.c
@@ -29,6 +29,19 @@ sched_yield(void)
 	// below to halt the cpu.
 
 	// LAB 4: Your code here.
+	int start = 0;
+	if (curenv) {
+		start = ENVX(curenv->env_id);
+	}
+	for (int i = 0; i < NENV; ++i) {
+		int index = (start + i) % NENV;
+		if (envs[index].env_status == ENV_RUNNABLE) {
+			env_run(&envs[index]);
+		}
+	}
+	if (curenv && curenv->env_status == ENV_RUNNING) {
+		env_run(curenv);
+	}
 
 	// sched_halt never returns
 	sched_halt();
