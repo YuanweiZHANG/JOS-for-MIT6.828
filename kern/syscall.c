c2cdbd9a3a7ba44f2e0e4407733f86aa2cff4fb9
YuanweiZHANG <veronica_zhang@pku.edu.cn>
2019-04-18 09:06:23 -0700

    Lab 4 Exercise 7

--- kern/syscall.c
+++ kern/syscall.c
@@ -85,6 +85,18 @@ sys_exofork(void)
 	// will appear to return 0.
 
 	// LAB 4: Your code here.
+	struct Env *fork;
+	int r = env_alloc(&fork, curenv->env_id);
+	if (r < 0) {
+		return r;
+	}
+
+	fork->env_status = ENV_NOT_RUNNABLE;
+	memcpy(&fork->env_tf, &curenv->env_tf, sizeof(struct Trapframe));
+	fork->env_tf.tf_regs.reg_eax = 0;  // Cautious: in child return 0
+
+	return fork->env_id;
+
 	panic("sys_exofork not implemented");
 }
 
@@ -105,6 +117,18 @@ sys_env_set_status(envid_t envid, int status)
 	// envid's status.
 
 	// LAB 4: Your code here.
+	struct Env *env;
+
+	if (envid2env(envid, &env, 1) < 0) {
+		return -E_BAD_ENV;
+	}
+
+	if (status != ENV_RUNNABLE && status != ENV_NOT_RUNNABLE) { // Cautious: this uses && not ||
+		return -E_INVAL;
+	}
+
+	env->env_status = status;
+	return 0;
 	panic("sys_env_set_status not implemented");
 }
 
@@ -150,6 +174,30 @@ sys_page_alloc(envid_t envid, void *va, int perm)
 	//   allocated!
 
 	// LAB 4: Your code here.
+	struct Env *env;
+	if (envid2env(envid, &env, 1) < 0) {
+		return -E_BAD_ENV;
+	}
+
+	if ((uintptr_t)va >= UTOP || (uintptr_t)va % PGSIZE != 0) {
+		return -E_INVAL;
+	}
+
+	if ((perm & (~PTE_SYSCALL)) || !(perm & (PTE_U | PTE_P))) {
+		return -E_INVAL;
+	}
+
+	struct PageInfo *pp = page_alloc(ALLOC_ZERO);
+	if (pp == NULL) {
+		return -E_NO_MEM;
+	}
+
+	if (page_insert(env->env_pgdir, pp, va, perm) != 0) {
+		page_decref(pp);
+		return -E_NO_MEM;
+	}
+
+	return 0;
 	panic("sys_page_alloc not implemented");
 }
 
@@ -181,6 +229,34 @@ sys_page_map(envid_t srcenvid, void *srcva,
 	//   check the current permissions on the page.
 
 	// LAB 4: Your code here.
+	struct Env *srcenv, *dstenv;
+	if (envid2env(srcenvid, &srcenv, 1) < 0 || envid2env(dstenvid, &dstenv, 1) < 0) {
+		return -E_BAD_ENV;
+	}
+
+	if ((uintptr_t)srcva >= UTOP || (uintptr_t)srcva % PGSIZE != 0 || (uintptr_t)dstva >= UTOP || (uintptr_t)dstva % PGSIZE != 0) {
+		return -E_INVAL;
+	}
+
+	pte_t *pte_pr;
+	struct PageInfo *pp = page_lookup(srcenv->env_pgdir, srcva, &pte_pr);
+	if (pp == NULL) {
+		return -E_INVAL;
+	}
+
+	if ((perm & (~PTE_SYSCALL)) || !(perm & (PTE_U | PTE_P))) {
+		return -E_INVAL;
+	}
+
+	if ((perm & PTE_W) && !(*pte_pr & PTE_W)) {
+		return -E_INVAL;
+	}
+
+	if (page_insert(dstenv->env_pgdir, pp, dstva, perm) != 0) {
+		return -E_NO_MEM;
+	}
+
+	return 0;
 	panic("sys_page_map not implemented");
 }
 
@@ -197,6 +273,21 @@ sys_page_unmap(envid_t envid, void *va)
 	// Hint: This function is a wrapper around page_remove().
 
 	// LAB 4: Your code here.
+	struct Env *env;
+	if (envid2env(envid, &env, 1) < 0) {
+		return -E_BAD_ENV;
+	}
+
+	if ((uintptr_t)va >= UTOP || (uintptr_t)va % PGSIZE !=0 ) {
+		return -E_INVAL;
+	}
+
+	struct PageInfo *pp = page_lookup(env->env_pgdir, va, NULL);
+	if (pp != NULL) {
+		page_remove(env->env_pgdir, va);
+	}
+
+	return 0;
 	panic("sys_page_unmap not implemented");
 }
 
@@ -289,6 +380,21 @@ syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, 
 		break;
 	case SYS_yield:
 		sys_yield();
+		break;
+	case SYS_exofork:
+		return sys_exofork();
+		break;
+	case SYS_env_set_status:
+		return sys_env_set_status((envid_t)a1, (int)a2);
+		break;
+	case SYS_page_alloc:
+		return sys_page_alloc((envid_t)a1, (void *)a2, (int)a3);
+		break;
+	case SYS_page_map:
+		return sys_page_map((envid_t)a1, (void *)a2, (envid_t)a3, (void *)a4, (int)a5);
+		break;
+	case SYS_page_unmap:
+		return sys_page_unmap((envid_t)a1, (void *)a2);
 		break;
 	default:
 		return -E_INVAL;
