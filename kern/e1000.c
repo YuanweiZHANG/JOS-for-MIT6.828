f6aed7f8e40ad34adfd16afd00874dfb35dd6f77
YuanweiZHANG <veronica_zhang@pku.edu.cn>
2019-06-20 10:49:24 -0700

    Lab 6 Exercise 4

--- kern/e1000.c
+++ kern/e1000.c
@@ -7,5 +7,8 @@ volatile uint32_t *e1000;
 
 int e1000_attach(struct pci_func *pcif) {
     pci_func_enable(pcif);
+    e1000 = mmio_map_region(pcif->reg_base[0], pcif->reg_size[0]);
+    cprintf("Exercise 4: status = 0x%x\n", e1000[E1000_STATUS]);
+    cprintf("           target is 0x80080783\n");
     return 0;
 }
--- kern/e1000.h
+++ kern/e1000.h
@@ -6,5 +6,8 @@
 #define E1000_VENDOR_ID  0x8086
 #define E1000_DEVICE_ID  0x100E
 
+// Cautious: e1000 MMIO address is uint32_t *, so must divided by sizeof(uint32_t)
+#define E1000_STATUS   0x00008/sizeof(uint32_t)  /* Device Status - RO */
+
 int e1000_attach(struct pci_func *pcif);
 #endif  // SOL >= 6
